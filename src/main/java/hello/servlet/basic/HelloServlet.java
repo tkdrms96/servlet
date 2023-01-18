c188e231ddb7c7f5e9d251899840ebfd407d53f7
전범진 <85546721+tkdrms96@users.noreply.github.com>
2023-01-18 23:37:06 +0900

    Initial commit

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// /helo로 들어왔을때 해당 servlet이 실행됨
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + request);
+        System.out.println("resp = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
